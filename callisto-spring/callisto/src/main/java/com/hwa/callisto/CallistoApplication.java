92560d43a62b1794a77009e2318a9b661f088780
yyubin <hazing120@gmail.com>
2023-03-24 15:49:34 +0900

    feat: Add index controller

---
+++ callisto-spring/callisto/src/main/java/com/hwa/callisto/CallistoApplication.java
@@ -0,0 +1,13 @@
+package com.hwa.callisto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallistoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallistoApplication.class, args);
+	}
+
+}
